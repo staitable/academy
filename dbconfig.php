6a3709114409092576f2f47203e786f4c5bc6d08
staitable <43377813+staitable@users.noreply.github.com>
2018-09-18 16:52:28 +0300

    database

--- dbconfig.php
+++ dbconfig.php
@@ -1,10 +1,10 @@
 <?php
 
 class dbconfig {
-	const DB_SERVER    = 'localhost';
-	const DB_NAME      = 'nfq';
-	const DB_USERNAME  = 'root';
-	const DB_PASSWORD  = '';
+	const DB_SERVER    = 'eu-cdbr-west-02.cleardb.net';
+	const DB_NAME      = 'heroku_bde3329cef2a499';
+	const DB_USERNAME  = 'b7c353d3c57464';
+	const DB_PASSWORD  = 'fb83cb20';
 	const DB_PREFIX	   = '';
 }
 
